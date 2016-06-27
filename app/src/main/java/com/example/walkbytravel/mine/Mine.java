0017b4193991827789d43de5a09e7ffad23f7cda
zfbzfb <1013497802@qq.com>
2016-06-27 10:12:29 +0800

    first commit

---
+++ app/src/main/java/com/example/walkbytravel/mine/Mine.java
@@ -0,0 +1,15 @@
+package com.example.walkbytravel.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.walkbytravel.R;
+
+public class Mine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
